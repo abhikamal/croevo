1b38ec95838516453492012116c1a88b563846e9
B Abhinav <141633895+abhikamal@users.noreply.github.com>
2026-02-10 22:16:58 +0530

    Delete croevo-site directory

--- croevo-site/js/supabase-config.js
+++
@@ -1,7 +0,0 @@
-// js/supabase-config.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabaseUrl = 'https://prlazkmlxpofsedpomqx.supabase.co'
-const supabaseKey = 'sb_publishable_6niljV54T0Z17h7kbqYe2A_ZP8dMWW5'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
